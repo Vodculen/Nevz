26cf505482fe16e88420fd7cdbfd584053563b3a
Vodculen <stefanaschen@gmail.com>
2025-07-17 12:41:39 -0400

    Added in utils.h and License

---
+++ src/include/utils.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void error(int status)
+{
+	switch (status)
+	{
+		case 1: 
+			printf("Too little parameters where given to compile the program\n");
+			break;
+		
+		// This should never be reached
+		default: 
+			printf("Unknown Error Status given, cannot provide any details\n");
+			break;
+	}
+
+	exit(status);
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "include/utils.h"
+
+int main(int argc, const char *argv[]) 
+{
+	error(1);
+
+	return 0;
+}
